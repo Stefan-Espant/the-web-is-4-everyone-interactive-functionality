4db053553f85e80f41a5e4079ae37982e841fec2
Stefan Espant <info@espant.com>
2023-03-30 10:44:03 +0200

    v2.1
    form plek reserveren opgemaakt.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Importeert belangrijke onderdelen
-import express, { request } from "express";
+import express, {
+	request
+} from "express";
 import dotenv from "dotenv";
 import fs from 'fs';
 
@@ -18,7 +20,9 @@ dotenv.config();
 
 // Stel afhandeling van formulieren in
 server.use(express.json())
-server.use(express.urlencoded({ extended: true }))
+server.use(express.urlencoded({
+	extended: true
+}))
 
 // Opbouw Boeken URL van de API
 const urlBase = "https://zoeken.oba.nl/api/v1/search/";
@@ -40,6 +44,7 @@ server.use(express.static("public"));
 
 // Maakt een route voor de overzichtspagina
 server.get("/", (request, response) => {
+
 	fetchJson(defaultUrl).then((data) => {
 		response.render("index", data);
 	});
@@ -56,102 +61,105 @@ server.get("/book", async (request, response) => {
 		.then((response) => response.json())
 		.catch((err) => err);
 	response.render("book", data);
-
 });
+
+
 
 // Maakt een route voor de reserveringspagina
 server.get("/reserveren", (request, response) => {
-    const baseurl = "https://api.oba.fdnd.nl/api/v1";
-
-    const url = `${baseurl}/reserveringen`;
+	const baseurl = "https://api.oba.fdnd.nl/api/v1";
+	const url = `${baseurl}/reserveringen`;
 
 	const reservation = request.query.reservations;
 
 	fetchJson(url).then((data) => {
 		response.render("reserveren", data);
 	});
-
-    fetchJson(defaultUrl).then((data) => {
-		response.render("reserveren", data);
-	});
 });
 
 // Verstuurt de data naar de API
 server.post("/reserveren", (request, response) => {
-    const baseurl = "https://api.oba.fdnd.nl/api/v1";
-    const url = `${baseurl}/reserveringen`;
+	const baseurl = "https://api.oba.fdnd.nl/api/v1";
+	const url = `${baseurl}/reserveringen`;
 
-    postJson(url, request.body).then((data) => {
-        let newReservation = { ...request.body };
+	postJson(url, request.body).then((data) => {
+		let newReservation = {
+			...request.body
+		};
 
-        if (data.success) {
-            response.redirect("/?reserveringenPosted=true");
-        } else {
-            const errormessage = `${data.message}: Mogelijk komt dit door het id die al bestaat.`;
-            const newdata = {
-                error: errormessage,
-                values: newReservation,
-            };
+		if (data.success) {
+			response.redirect("/?reserveringenPosted=true");
+		} else {
+			const errormessage = `${data.message}: Mogelijk komt dit door het id die al bestaat.`;
+			const newdata = {
+				error: errormessage,
+				values: newReservation,
+			};
 
-            response.render("reserveren", newdata);
-        }
+			response.render("reserveren", newdata);
+		}
 
-        console.log(JSON.stringify(data.errors))
-    });
+		console.log(JSON.stringify(data.errors))
+	});
 
 });
 
 // Maakt een route voor de profielpagina
-server.get("/profile"),
-	async (request, response) => {
-		fetchJson(defaultUrl).then((data) => {
-			response.render("profile", data);
-		});
-	};
+server.get("/profile"), (request, response) => {
+		response.render("profile");
+};
+
+// Maakt een route voor de plekreservering 
+server.get("/reserveer-een-plek",(request, response) => {
+		response.render("reserveer-een-plek");
+});
+
+server.post("/reserveer-een-plek",(request, response) => {
+	response.render("reserveer-een-plek");
+});
+
+
 
 // Definieer gebruikersdata als een array in een JSON-bestand
-let usersData = JSON.parse(fs.readFileSync('./users.json','utf8'));
+let usersData = JSON.parse(fs.readFileSync('./users.json', 'utf8'));
 
-// Definieer de routes voor de applicatie
-server.get('/', (req, res) => {
-  res.render('index');
+server.get('/login', (req, response) => {
+	response.render('login');
 });
 
-server.get('/login', (req, res) => {
-  res.render('login');
+server.post('/login', (request, response) => {
+	const {
+		email,
+		password
+	} = request.body;
+
+	const user = usersData.find((u) => u.email === email);
+
+	if (!user || user.password !== password) {
+		return response.status(401).send('Onjuiste inloggegevens');
+	}
+
+	response.redirect('/');
 });
 
-server.post('/login', (req, res) => {
-  const { email, password } = req.body;
-
-  const user = usersData.find((u) => u.email === email);
-
-  if (!user || user.password !== password) {
-    return res.status(401).send('Onjuiste inloggegevens');
-  }
-
-  res.redirect('/');
-});
-
-server.get('/logout', (req, res) => {
-  // Hier zou normaal gesproken de cookie met het JSON web token worden verwijderd
-  // Maar omdat we geen 'jsonwebtoken' module gebruiken, laten we deze stap weg
-
-  res.redirect('/login');
+server.get('/logout', (req, response) => {
+	response.redirect('/login');
 });
 
 // Stelt afhandeling van formulieren in
 server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
+server.use(express.urlencoded({
+	extended: true
+}));
 
 // Start express op, haal het ingestelde poortnummer op
 server.listen(server.get("port"), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(
-    `Application started on http://localhost:${server.get(
+	// Toon een bericht in de console en geef het poortnummer door
+	console.log(
+		`Application started on http://localhost:${server.get(
       "port"
     )}`
-  );
+	);
 });
 
 
@@ -163,10 +171,12 @@ async function fetchJson(url, payload = {}) {
 
 export async function postJson(url, body) {
 	return await fetch(url, {
-		method: "post",
-		body: JSON.stringify(body),
-		headers: { "Content-Type": "application/json" },
-	})
+			method: "post",
+			body: JSON.stringify(body),
+			headers: {
+				"Content-Type": "application/json"
+			},
+		})
 		.then((response) => response.json())
 		.catch((error) => error);
 }
